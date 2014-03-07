eaf0b4e1532b4100ce51a5779792eeeed9264144
Quin Cogdell <quincogdell@gmail.com>
2014-03-07 12:30:21 -0500

    delete scope

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -1,5 +1,15 @@
 'use strict';
 
 app.controller('PostsCtrl', function($scope){
+  $scope.posts = [];
+  $scope.post = {url:'http://', title:'Hello'};
 
+  $scope.submitPost = function() {
+    $scope.posts.push($scope.post);
+    $scope.post = {url: 'http://', title: ''};
+  };
+
+  $scope.deletePost = function(index) {
+    $scope.posts.splice(index, 1);
+  }
 });
