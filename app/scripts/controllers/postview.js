1251b366467c907514d7c4f2d1e24c9d65a83acc
Quin Cogdell <quincogdell@gmail.com>
2014-03-11 12:18:34 -0400

    end of video 8 part 2

--- app/scripts/controllers/postview.js
+++ app/scripts/controllers/postview.js
@@ -4,4 +4,12 @@ app.controller('PostViewCtrl', function($scope, $routeParams, Post){
 
   $scope.post = Post.find($routeParams.postId);
 
+  $scope.addComment = function() {
+    Post.addComment($routeParams.postId, $scope.comment);
+    $scope.comment = {};
+  };
+
+  $scope.removeComment = function(comment, commentId) {
+    Post.deleteComment($scope.post, comment, commentId);
+  }
 });
--- app/scripts/services/posts.js
+++ app/scripts/services/posts.js
@@ -1,51 +1,135 @@
 'use strict';
 
-app.factory('Post', function($firebase, FIREBASE_URL, User) {
-  // return $resource('https://fiery-fire-7125.firebaseio.com/posts.json');
-  // return $resource('https://fiery-fire-7125.firebaseio.com/posts/:id.json');
+app.factory('Post',
+  function ($firebase, User, FIREBASE_URL) {
+    var ref = new Firebase(FIREBASE_URL + 'posts');
 
-  // adding 3 way data binding
-  var ref = new Firebase (FIREBASE_URL + 'posts');
+    var posts = $firebase(ref);
 
-  var posts = $firebase (ref);
+    var Post = {
+      all: posts,
+      create: function (post) {
+        if (User.signedIn()) {
+          var user = User.getCurrent();
 
-  var Post = {
-    all: posts,
-    create: function (post){
-      if (User.signedIn()) {
-        var user = User.getCurrent();
+          post.owner = user.username;
 
-        post.owner = user.username
+          return posts.$add(post).then(function (ref) {
+            var postId = ref.name();
 
-        return posts.$add(post).then(function (ref) {
-          var postId = ref.name();
+            user.$child('posts').$child(postId).$set(postId);
 
-          user.$child('posts').$child(postId).$set(postId);
+            return postId;
+          });
+        }
+      },
+      find: function (postId) {
+        return posts.$child(postId);
+      },
+      delete: function (postId) {
+        if (User.signedIn()) {
+          var post = Post.find(postId);
 
-          return postId;
-        })
+          post.$on('loaded', function () {
+            var user = User.findByUsername(post.owner);
+
+            posts.$remove(postId).then(function () {
+              user.$child('posts').$remove(postId);
+            });
+          });
+        }
+      },
+      addComment: function (postId, comment) {
+        if (User.signedIn()) {
+          var user = User.getCurrent();
+
+          comment.username = user.username;
+          comment.postId = postId;
+
+          posts.$child(postId).$child('comments').$add(comment).then(function (ref) {
+            user.$child('comments').$child(ref.name()).$set({id: ref.name(), postId: postId});
+          });
+        }
+      },
+      deleteComment: function (post, comment, commentId) {
+        if (User.signedIn()) {
+          var user = User.findByUsername(comment.username);
+
+          post.$child('comments').$remove(commentId).then(function () {
+            user.$child('comments').$remove(commentId);
+          });
+        }
+      },
+      upVote: function (postId) {
+        if (User.signedIn()) {
+          var user = User.getCurrent();
+          var post = posts.$child(postId);
+
+          post.$child('upvotes').$child(user.username).$set(user.username).then(function () {
+              user.$child('upvotes').$child(postId).$set(postId);
+              post.$child('downvotes').$remove(user.username);
+              user.$child('downvotes').$remove(postId);
+
+              post.$child('score').$transaction(function (score) {
+                if (!score) {
+                  return 1;
+                }
+
+                return score + 1;
+              });
+            });
+        }
+      },
+      downVote: function (postId) {
+        if (User.signedIn()) {
+          var user = User.getCurrent();
+          var post = posts.$child(postId);
+
+          post.$child('downvotes').$child(user.username).$set(user.username).then(function () {
+              user.$child('downvotes').$child(postId).$set(postId);
+              post.$child('upvotes').$remove(user.username);
+              user.$child('upvotes').$remove(postId);
+
+              post.$child('score').$transaction(function (score) {
+                if (score === undefined || score === null) {
+                  return -1;
+                }
+
+                return score - 1;
+              });
+            });
+        }
+      },
+      clearVote: function (postId, upVoted) {
+        if (User.signedIn()) {
+          var user = User.getCurrent();
+          var username = user.username;
+          var post = posts.$child(postId);
+
+          post.$child('upvotes').$remove(username);
+          post.$child('downvotes').$remove(username);
+          user.$child('upvotes').$remove(postId);
+          user.$child('downvotes').$remove(postId);
+          post.$child('score').$transaction(function (score) {
+            if (upVoted) {
+              return score - 1;
+            } else {
+              return score + 1;
+            }
+          });
+        }
+      },
+      upVoted: function (post) {
+        if (User.signedIn() && post.upvotes) {
+          return post.upvotes.hasOwnProperty(User.getCurrent().username);
+        }
+      },
+      downVoted: function (post) {
+        if (User.signedIn() && post.downvotes) {
+          return post.downvotes.hasOwnProperty(User.getCurrent().username);
+        }
       }
-      // coming from angularfire library $add, $chile, $remove
-      return posts.$add(post);
-    },
-    find: function(postId) {
-      return posts.$child(postId);
-    },
-    delete: function(postId) {
-      if (User.signedIn()) {
-        var post = Post.find(postId);
+    };
 
-        post.$on('loaded', function () {
-          var user = User.findByUsername(post.owner);
-
-          posts.$remove(postId).then(function() {
-            user.$child('posts').$remove(postId);
-          })
-        })
-      }
-      return posts.$remove(postId);
-    }
-  };
-
-  return Post;
-});
+    return Post;
+  });
